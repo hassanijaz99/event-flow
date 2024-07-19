110eded7d012b14e157219e0f051014d2ab533b4
hassanijaz99 <hassan.ijaz3336@gmail.com>
2024-07-19 17:22:56 +0500

    Update SelectPicker.tsx

--- src/app/components/create-event/SelectPicker.tsx
+++ src/app/components/create-event/SelectPicker.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import useGlobalContext from '@/app/hooks/useGlobalContext';
 
 type Props = {
-    items: array;
+    items: Array;
     lightIcon: string;
     darkIcon: string;
     label: string;
